10d407b3ced882241383563a0668d9a8c4450ed3
Aaditya Pratap <aadityapratapiit@gmail.com>
2026-01-14 16:02:24 +0530

    updated db for system design

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,12 @@ import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
 export const connectDB = async () => {
+  // Check if we already have a ready connection
+  if (mongoose.connection.readyState === 1) {
+    console.log("MONGODB already connected");
+    return;
+  }
+
   try {
     const conn = await mongoose.connect(ENV.MONGO_URI);
     console.log("MONGODB connected successfully", conn.connection.host);
